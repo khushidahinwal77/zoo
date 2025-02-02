66771556258599b83a6d61ed6ac88ee3f7a015bf
khushidahinwal77 <148880006+khushidahinwal77@users.noreply.github.com>
2025-02-02 13:41:58 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,8 @@
+package pac1;
+public class A{
+public void display()
+{
+
+System.out.print("nested packages");
+}
+}
---
+++ B.java
@@ -0,0 +1,8 @@
+package pac2;
+class B {
+public static void main(String st[])
+{
+pac1.A obj=new pac1.A();
+obj.display();
+}
+}
